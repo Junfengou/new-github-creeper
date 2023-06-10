7f3d0f5b650c337aefb951548f898a740b1389da
Junfeng Ou <jun@Junfengs-MacBook-Pro.local>
2023-06-10 09:57:50 -0500

    mui and typography are working

--- src/pages/NewPage.tsx
+++ src/pages/NewPage.tsx
@@ -1,11 +1,15 @@
 import Header from '@/helpers/Header'
 import React from 'react'
+import Icon from '@mui/material/Icon'
+import Button from "@mui/material/Button"
 
 function NewPage() {
   return (
     <>
         <Header />
-        <div>NewPage</div>
+        <div>
+        <Button onClick={() => console.log("hello world")} variant='contained'>Hello World</Button>
+        </div>
     </>
   )
 }
---
+++ src/pages/_app.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { AppProps } from "next/app"
+// import { RecoilRoot } from "recoil"
+import '../app/globals.css'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#558a55'
+      }
+    },
+    typography: {
+      fontFamily: [
+        'FrenchFries'
+      ].join(',')
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
